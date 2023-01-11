2e4a6cbf68a3268908297f0d436ded8f1408e207
chuu100 <106085848+chuu100@users.noreply.github.com>
2023-01-11 22:21:00 +0900

    Update RestartServlet.java

--- src/main/java/com/tictactoe/RestartServlet.java
+++ src/main/java/com/tictactoe/RestartServlet.java
@@ -1,1 +1,14 @@
-package com.tictactoe;import javax.servlet.http.*;import javax.servlet.annotation.*;import java.io.IOException;@WebServlet(name = "RestartServlet", value = "/restart")public class RestartServlet extends HttpServlet {    @Override    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {        req.getSession().invalidate();        resp.sendRedirect("/start");    }}
+package com.tictactoe;
+
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RestartServlet", value = "/restart")
+public class RestartServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.getSession().invalidate();
+        resp.sendRedirect("/start");
+    }
+}
